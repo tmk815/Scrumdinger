93a5519660257ac67944cf08dd473c7903bbe3ce
Tomoki Monden <t_monden@yumemi.co.jp>
2021-04-05 13:44:03 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 門田 朋己 on 2021/04/05.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
