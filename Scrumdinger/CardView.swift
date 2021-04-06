bfca8c4a575a25843b119f4b6aae6535c05ea733
Tomoki Monden <t_monden@yumemi.co.jp>
2021-04-06 14:32:11 +0900

    プレビューの背景色を指定し、修飾子を使用してプレビューサイズを修正

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -18,5 +18,7 @@ struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.data[0]
     static var previews: some View {
         CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
