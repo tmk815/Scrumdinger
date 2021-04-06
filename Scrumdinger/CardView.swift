897f2b001747b906c127958cefe600ffe07901b7
Tomoki Monden <t_monden@yumemi.co.jp>
2021-04-06 14:28:53 +0900

    CardViewの作成

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by 門田 朋己 on 2021/04/06.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
