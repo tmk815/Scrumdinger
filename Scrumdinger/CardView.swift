86ff98882f4a85b1b82136c80ec04041afb7498b
Tomoki Monden <t_monden@yumemi.co.jp>
2021-04-06 15:01:23 +0900

    アクセシビティ関連を追加

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -15,22 +15,27 @@ struct CardView: View {
                 .font(.headline)
             Spacer()
             HStack {
-                Label("\(scrum.attendees.count)",systemImage: "person.3")
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Attendees"))
+                    .accessibilityValue(Text("\(scrum.attendees.count)"))
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .padding(.trailing, 20)
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Meeting length"))
+                    .accessibilityValue(Text("\(scrum.lengthInMinutes) minutes"))
             }
             .font(.caption)
         }
         .padding()
-        .foregroundColor(scrum.color.accessibleFontColor)
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.data[0]
     static var previews: some View {
-        CardView(scrum: scrum)
+        CardView(scrum: DailyScrum.data[0])
             .background(scrum.color)
             .previewLayout(.fixed(width: 400, height: 60))
     }
